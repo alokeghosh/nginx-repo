88347174b3be876bf2f5565299aae7b2db253984
Aloke <47846743+alokeghosh@users.noreply.github.com>
2023-07-06 15:50:42 +0530

    Update install_scripts.sh

--- install_scripts.sh
+++ install_scripts.sh
@@ -1,5 +1,6 @@
 mkdir -p /home/azureadmin/aloke/ghosh/
 sudo apt-get  install nginx -y
-sudo echo "-----------------Welcome To my green project|| file edited form dev ------------------" > /usr/share/nginx/html/index.html
+sudo chmod 666 /usr/share/nginx/html/index.html
+sudo echo "-----------------Welcome To my green project------------------" > /usr/share/nginx/html/index.html
 echo "-----------------Welcome To RED Nginx WebSite ONE for devops blnlknzkCNxzNCxzCnZXCNk ------------------" > /home/azureadmin/aloke/ghosh/test
 
