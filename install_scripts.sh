26247c8cbb1c102dcc4283382b09a79d9fb5f915
Aloke <47846743+alokeghosh@users.noreply.github.com>
2023-07-06 15:30:09 +0530

    Update install_scripts.sh

--- install_scripts.sh
+++ install_scripts.sh
@@ -1,2 +1,2 @@
-mkdir -P /home/azureadmin/aloke/ghosh/
+mkdir -p /home/azureadmin/aloke/ghosh/
 echo "-----------------Welcome To RED Nginx WebSite ONE for devops blnlknzkCNxzNCxzCnZXCNk ------------------" > /home/azureadmin/aloke/ghosh/test
