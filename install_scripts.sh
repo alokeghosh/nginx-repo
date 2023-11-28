ff21c35985d2ce4198dffa83dc71e2c1ef1b1981
Aloke <47846743+alokeghosh@users.noreply.github.com>
2023-11-28 16:19:14 +0530

    Update install_scripts.sh

--- install_scripts.sh
+++ install_scripts.sh
@@ -1,7 +1,1 @@
-mkdir -p /home/azureadmin/aloke/ghosh/
-sudo apt-get  install nginx -y
-sudo apt install vsftpd -y 
-sudo chmod 666 /var/www/html/index.nginx-debian.html
-sudo echo "-----------------Welcome To my green project----Aloke mkmkmkmkmkmk ------------------" > /var/www/html/index.nginx-debian.html
-echo "-----------------Welcome To RED Nginx WebSite ONE for kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk ------------------" > /home/azureadmin/aloke/ghosh/test
-
+az aks start --name poc-aks --resource-group poc-rg
