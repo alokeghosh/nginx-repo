2f17fba7620bd3847ae00085ae4d7f91d700512c
Aloke <47846743+alokeghosh@users.noreply.github.com>
2023-12-02 14:13:08 +0530

    Update install_scripts.sh

--- install_scripts.sh
+++ install_scripts.sh
@@ -1,1 +1,2 @@
+az login --service-principal --username 3b443520-d11f-4bcc-b507-2fd9a0db94d9 --password LFk8Q~rRWpKhmnGEDJwb~Zba67nGnwfJpQioUcFR --tenant 718a9912-4d8d-4d9d-af9a-abe3dca42cb2
 az aks start --name poc-aks --resource-group poc-rg
